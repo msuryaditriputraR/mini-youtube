0acf55b15dfca35d171e95d8c3499522de06e571
Muh Suryadi Triputra Rahman <msuryaditriputra@gmail.com>
2022-04-01 18:11:16 +0800

    update card component

--- src/components/card.js
+++ src/components/card.js
@@ -1,11 +1,19 @@
-export default function Card({ thumbnail, title, desc, avatar, name, subs }) {
+export default function Card({
+    thumbnail,
+    title,
+    desc,
+    avatar,
+    name,
+    subs,
+    video
+}) {
     const cardName = document.createElement('h3');
     cardName.className = 'card__name';
     cardName.innerText = name;
 
     const cardSubs = document.createElement('p');
     cardSubs.className = 'card__subs';
-    cardSubs.innerText = subs;
+    cardSubs.innerText = `subs : ${subs}`;
 
     const cardDetail = document.createElement('div');
     cardDetail.append(cardName, cardSubs);
@@ -30,13 +38,25 @@ export default function Card({ thumbnail, title, desc, avatar, name, subs }) {
     cardBody.className = 'card__body';
     cardBody.append(cardTitle, cardDesc, cardChannel);
 
-    const cardThumbnail = document.createElement('img');
-    cardThumbnail.src = thumbnail;
-    cardThumbnail.className = 'card__thumbnail';
-
     const card = document.createElement('div');
     card.className = 'card';
-    card.append(cardThumbnail, cardBody);
+
+    if (thumbnail) {
+        const cardThumbnail = document.createElement('img');
+        cardThumbnail.src = thumbnail;
+        cardThumbnail.className = 'card__thumbnail';
+
+        card.append(cardThumbnail);
+    }
+
+    if (video) {
+        const div = document.createElement('div');
+        div.innerHTML = video;
+
+        card.append(div);
+    }
+
+    card.append(cardBody);
 
     return card;
 }
